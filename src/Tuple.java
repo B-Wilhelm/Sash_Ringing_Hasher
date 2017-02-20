fcb44ff76708cb7fccba81fa43569d9f188c10f9
Brett <bwilhelm@iastate.edu>
2017-02-20 12:09:43 -0600

    Tuple class finished

--- src/Tuple.java
+++ src/Tuple.java
@@ -1,8 +1,26 @@
-/*
- * 
+/**
+ * 	@author	Brett W.
  */
 
 public class Tuple {
-
+	int key;
+	float value;
+	
+	Tuple(int keyP, float valueP) {
+		key = keyP;
+		value = valueP;
+	}
+	
+	int getKey() {
+		return key;
+	}
+	
+	float getValue() {
+		return value;
+	}
+	
+	boolean equals(Tuple t) {
+		return (value == t.value);
+	}
 }
 
